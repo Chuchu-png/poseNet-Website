5fadd0512ce8c2a0dd1a4f76981f07618bd8f53c
Chuchu-png <56744420+Chuchu-png@users.noreply.github.com>
2021-03-19 19:17:57 -1000

    Add files via upload

--- poseNet.js
+++ poseNet.js
@@ -18,115 +18,74 @@
 */
 
 
-
+let video;
+let poseNet;
+let pose;
+let skeleton;
 var script= []; //empty array that everything is going to be pushed to 
-// Grab elements, create settings, etc.
-var video = document.getElementById("video");
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-// The detected positions will be inside an array
 let poses = [];
-let critcism = [];
 
-
-// Create a webcam capture
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    video.srcObject = stream;
-    video.play();
-    });
-  }
-
-// A function to draw the video and poses into the canvas.
-// This function is independent of the result of posenet
-// This way the video will not seem slow if poseNet
-// is not detecting a position
-function drawCameraIntoCanvas() {
-  // Draw the video element into the canvas
-  ctx.drawImage(video, 0, 0, video.width, video.height);
-
-  // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawSkeleton();
-  window.requestAnimationFrame(drawCameraIntoCanvas);
-  
+function setup() {
+    createCanvas(640, 480);
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-// Loop over the drawCameraIntoCanvas function
-drawCameraIntoCanvas();
 
-// Create a new poseNet method with a single detection
-const poseNet = ml5.poseNet(video, modelReady);
-poseNet.on("pose", gotPoses);
+function gotPoses(poses) {
+    //console.log(poses);
+    if(poses.length > 0) {
+        pose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
+}
 
-// A function that gets called every time there's an update from the model
-function gotPoses(results) {
+function modelLoaded() {
+    //console.log("posenet ready");
+}
 
-poses = results;
-  if(poses.length> 0){
-  var rightwristX = poses[0].pose.keypoints[10].position.x;
-  var leftwristX = poses[0].pose.keypoints[9].position.x;
+function draw() {
+    image(video, 0, 0);
+    
+    if(pose) {
+    var rightwristY = pose.keypoints[10].position.y
+    var leftwristY = pose.keypoints[10].position.y
+    var rightankleX = pose.keypoints[16].position.y
 
-   //rightwristX critcism
-  if (rightwristX > 200) {
-  console.log("move to right wrist to the right"); 
-   var critcism1 = "move your right wrist to the right";
-    /*critcism.push(critcism1);
-    document.getElementById("blah").innerHTML = critcism[0];*/
-  }
-
-  else {
-        console.log("great!");
-  }
-
-  if (leftwristX > 300) {
-        console.log("move your left wrist to the right");
-  }
-  
-  else if(leftwristX < 50) {
-        console.log("move your left wrist to the left");
-  }
-
-  else {
-        console.log("great!");
+    for(let i = 0; i < pose.keypoints.length; i++) {
+        let x = pose.keypoints[i].position.x;
+        let y = pose.keypoints[i].position.y;
+        fill(255, 255, 255);
+        ellipse(x,y,16,16);
         }
-    }
-}
 
+        for(let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(1);
+      stroke(255);
+      line(a.position.x, a.position.y, b.position.x, b.position.y);
+        }
 
-function modelReady() {
-  console.log("model ready");
-  poseNet.multiPose(video);
-}
-
-// A function to draw ellipses over the detected keypoints
-function drawKeypoints() {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For each pose detected, loop through all the keypoints
-    for (let j = 0; j < poses[i].pose.keypoints.length; j += 1) {
-      let keypoint = poses[i].pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        ctx.beginPath();
-        ctx.arc(keypoint.position.x, keypoint.position.y, 12, 0, 2 * Math.PI);
-        ctx.stroke();
-      }
-    }
+   if (rightwristY > 200 && leftwristY > 200) {
+    document.getElementById("blah").innerHTML = "move your left and right wrist up";
   }
-}
 
-// A function to draw the skeletons
-function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i += 1) {
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < poses[i].skeleton.length; j += 1) {
-      let partA = poses[i].skeleton[j][0];
-      let partB = poses[i].skeleton[j][1];
-      ctx.beginPath();
-      ctx.moveTo(partA.position.x, partA.position.y);
-      ctx.lineTo(partB.position.x, partB.position.y);
-      ctx.stroke();
-    }
+  else {
+    document.getElementById("blah").innerHTML = "great!";
   }
+//console.log(rightankleX);
+ if (rightankleX < 250) {
+      document.getElementById("blah2").innerHTML = "move your right leg down";
+ } 
+
+ else if (rightankleX > 350) {
+    document.getElementById("blah2").innerHTML = "move your right leg up"
+        }
+
+    else {
+        document.getElementById("blah2").innerHTML = "great!";
+       }
+    }
 }
